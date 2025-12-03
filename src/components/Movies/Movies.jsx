5cfb88f21a733d536d003aadf4b105fa8e7c736e
Kolcman <m.Kolcman@yandex.ru>
2025-12-03 16:49:04 +0300

    Hide Api key

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,13 +2,15 @@ import MovieCard from "../MovieCard/MovieCard";
 import styles from "./Movies.module.css";
 
 export default function Movies(props) {
-  const { movies } = props;
+  const { movies = [] } = props;
 
   return (
     <div className={`${styles.movies}`}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.imdbID} {...movie} />
-      ))}
+      {movies.length ? (
+        movies.map((movie) => <MovieCard key={movie.imdbID} {...movie} />)
+      ) : (
+        <h4>Nothing found</h4>
+      )}
     </div>
   );
 }
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -3,37 +3,41 @@ import Movies from "../components/Movies/Movies";
 import Preloader from "../components/Preloader/Preloader";
 import Search from "../components/Search/Search";
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 export default class Main extends Component {
   state = {
     movies: [],
+    loading: true,
   };
 
   componentDidMount() {
-    fetch("http://www.omdbapi.com/?apikey=e4adf7db&s=matrix")
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
 
   searchMovies = (str, type) => {
+    this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=e4adf7db&s=${str}${
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   };
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     return (
       <main className="container content">
         <Search searchMovies={this.searchMovies} />
-        {movies.length ? (
-          <Movies movies={this.state.movies} />
-        ) : (
+        {loading ? (
           <Preloader className="prealoader" />
+        ) : (
+          <Movies movies={movies} />
         )}
       </main>
     );
