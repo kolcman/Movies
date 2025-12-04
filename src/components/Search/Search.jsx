b8541a3b8d5e7337a2d841880a7385207d68e5eb
Kolcman <m.Kolcman@yandex.ru>
2025-12-04 15:22:57 +0300

    refactorinc class to functional

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,101 +1,92 @@
-import { Component } from "react";
+import { useState } from "react";
 import styles from "./Search.module.css";
 
-export default class Search extends Component {
-  state = {
-    search: "",
-    type: "",
-  };
+export default function Search(props) {
+  const { searchMovies = Function.prototype } = props;
+  const [search, setSearch] = useState("");
+  const [type, setType] = useState("");
 
-  handleKey = (event) => {
+  const handleKey = (event) => {
     if (event.key === "Enter") {
-      this.props.searchMovies(this.state.search, this.state.type);
+      searchMovies(search, type);
     }
   };
 
-  handleFilter = (e) => {
-    this.setState(
-      () => ({ type: e.target.dataset.type }),
-      () => {
-        this.props.searchMovies(this.state.search, this.state.type);
-      }
-    );
+  const handleFilter = (e) => {
+    setType(e.target.dataset.type);
+    searchMovies(search, e.target.dataset.type);
   };
 
-  render() {
-    return (
-      <>
-        <div className={`${styles.search}`}>
-          <div className={`"input-field" ${styles.search__input}`}>
-            <input
-              className="validate"
-              id="email_inline"
-              type="search"
-              placeholder="Search"
-              value={this.state.search}
-              onChange={(e) => this.setState({ search: e.target.value })}
-              onKeyDown={this.handleKey}
-            />
-          </div>
-          <button
-            className={`${styles.search__btn}`}
-            onClick={() =>
-              this.props.searchMovies(this.state.search, this.state.type)
-            }
-          >
-            Search
-          </button>
+  return (
+    <>
+      <div className={`${styles.search}`}>
+        <div className={`"input-field" ${styles.search__input}`}>
+          <input
+            className="validate"
+            id="email_inline"
+            type="search"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={handleKey}
+          />
         </div>
-        <div className={`${styles.radio__group}`}>
-          <label className={`${styles.radio__label}`}>
-            <input
-              className="with-gap"
-              name="type"
-              type="radio"
-              data-type="all"
-              onChange={this.handleFilter}
-              checked={this.state.type === "all"}
-            />
-            <span>All</span>
-          </label>
+        <button
+          className={`${styles.search__btn}`}
+          onClick={() => searchMovies(search, type)}
+        >
+          Search
+        </button>
+      </div>
+      <div className={`${styles.radio__group}`}>
+        <label className={`${styles.radio__label}`}>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="all"
+            onChange={handleFilter}
+            checked={type === "all"}
+          />
+          <span>All</span>
+        </label>
 
-          <label className={`${styles.radio__label}`}>
-            <input
-              className="with-gap"
-              name="type"
-              type="radio"
-              data-type="movie"
-              onChange={this.handleFilter}
-              checked={this.state.type === "movie"}
-            />
-            <span>Movies</span>
-          </label>
+        <label className={`${styles.radio__label}`}>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="movie"
+            onChange={handleFilter}
+            checked={type === "movie"}
+          />
+          <span>Movies</span>
+        </label>
 
-          <label className={`${styles.radio__label}`}>
-            <input
-              className="with-gap"
-              name="type"
-              type="radio"
-              data-type="series"
-              onChange={this.handleFilter}
-              checked={this.state.type === "series"}
-            />
-            <span>Series</span>
-          </label>
+        <label className={`${styles.radio__label}`}>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="series"
+            onChange={handleFilter}
+            checked={type === "series"}
+          />
+          <span>Series</span>
+        </label>
 
-          <label className={`${styles.radio__label}`}>
-            <input
-              className="with-gap"
-              name="type"
-              type="radio"
-              data-type="game"
-              onChange={this.handleFilter}
-              checked={this.state.type === "game"}
-            />
-            <span>Games</span>
-          </label>
-        </div>
-      </>
-    );
-  }
+        <label className={`${styles.radio__label}`}>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            data-type="game"
+            onChange={handleFilter}
+            checked={type === "game"}
+          />
+          <span>Games</span>
+        </label>
+      </div>
+    </>
+  );
 }
